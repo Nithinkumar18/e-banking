ed1986f42036ca05d9baec350ca6817dac902b11
Nithin <vooturinithinkumar@gmail.com>
2025-01-16 13:15:17 +0530

    add constants, info des

---
+++ src/constants/operationdes.js
@@ -0,0 +1,40 @@
+const operation_description = {
+    USER_REGESTERED: "User Regestration Success",
+    INVALID_USER: "valid user doesn't exists with provided customerId",
+    USER_INFO_UPDATE: "user personal details updated",
+    OPEN_ACCOUNT: "Congratulations, you have opened your account",
+    SUCCESS_INFO: "success",
+    FAIL_INFO: "fail",
+    ERROR_INFO: "error",
+    VIEW_USER_INFO_ERR: "View user details operation failed",
+    USER_REGES_FAIL: "User Regestration Fail",
+    USER_INFO_UPDATE: "user details updated",
+    USER_INFO_FAIL: "user details update failed",
+    USER_UNREGISTER_SUCCESS: "user unregister success",
+    USER_UNREGISTER: "user unregister fail",
+    USER_CREDENTIALS_INVALID: "Invalid email / Password, Try Again!",
+    INVALID_TOKEN_FORMAT: "Invalid token format / No Token Found",
+    TOKEN_EXPIRED: "Token Expired.To Resume, Please login",
+    TOKEN_VALIDATION_FAILED: "token validation failed",
+    USER_LOGIN_FAILED: "user login failed",
+    OPEN_ACCOUNT_INFO: "Congratulations! Your new account at Citi Bank has been successfully opened.",
+    ACC_NOT_FOUND: "Account doesn't exist with given customerid",
+    ACC_ERR_INFO: "Error fetching user account info",
+    ACC_CRTE_ERR: "Could not create new account,Please Try Again Later",
+    MINIMUM_BALANCE: "your account should hold minimum balance of 1100, you are not allowed to perform withdraw",
+    TRANSACTION_WITHDRAW_SUCCESS: "Your withdrawal was successful. Thank you for banking with us!",
+    TRANSACTION_WITHDRAW_FAIL: "Your withdrawal was Failed ",
+    TRANSACTION_DEPOSIT_FAIL: "Your Deposit was Failed",
+    TRANSACTION_DEPOSIT_SUCCESS: "Your deposit was successful. Thank you for banking with us!",
+    TRANSFER_D_TYPE: "Debit",
+    TRANSFER_C_TYPE: "Credit",
+    TRANSFER_SELF_TYPE: "Within Account Transaction",
+    TRANSFER_STATUS: "Success",
+    TRANSFER_F_STATUS: "Failed",
+    MINIMUM_BALANCE_NUM:1100
+
+
+}
+
+
+module.exports = operation_description
---
+++ src/constants/operationstatus.js
@@ -0,0 +1,12 @@
+
+const status_code = {
+    SUCCESS: 200,
+    INTERNAL_SERVER_ERROR: 500,
+    NOT_FOUND: 404,
+    RECORD_CREATED:201,
+    UNAUTHORISED_REQUEST: 401,
+    FAIL: 400
+
+}
+
+module.exports = status_code;
