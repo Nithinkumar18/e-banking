936327534336b35af721628d5b16f721ad40d2fc
Nithin <vooturinithinkumar@gmail.com>
2025-01-16 13:13:45 +0530

    implement connection to db, start server

---
+++ app.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = express();
+const accountController = require('./src/controller/accountController');
+const userController = require('./src/controller/userController');
+
+dotenv.config();
+app.use(express.json());
+app.use('/api/user',userController);
+app.use('/api/account',accountController);
+const connectTime = new Date().toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', second: '2-digit'});
+ mongoose.connect('mongodb://localhost:27017/e-banking', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        app.listen(8080);
+        console.log(`Connected to DB and app running on PORT ${process.env.PORT}`);
+        console.log(`connection time: ${connectTime}`);
+    })
+    .catch(err => {
+        console.log("error connecting to DB", err);
+    });
